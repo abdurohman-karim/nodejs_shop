77b9ef7da2f1d0dc60d6b783e822e5a90108b05f
Abdurohman Karim <ghostmagic766@gmail.com>
2024-02-10 12:08:40 +0500

    handlebars installed

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {engine} from 'express-handlebars';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
