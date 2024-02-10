d6f921fff54a05d9c095090333f117eda839d700
Abdurohman Karim <ghostmagic766@gmail.com>
2024-02-10 10:53:20 +0500

    updated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 4100;
+const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
