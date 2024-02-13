c355c4f3ae6d193b5cdfc38576ac8a9c10ac13ca
Abdurohman Karim <ghostmagic766@gmail.com>
2024-02-13 09:28:00 +0500

    dynamic routes

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,24 @@ const router = Router();
 
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Home | My Store',
+        isIndex: true
+    })
 })
 
 router.get('/products', (req, res) => {
-    res.render('products')
+    res.render('products', {
+        title: 'Products | My Store',
+        isProduct: true
+    })
+})
+
+router.get('/add-product', (req, res) => {
+    res.render('add-product', {
+        title: 'Add Product | My Store',
+        isProductAdd: true
+    })
 })
 
 export default router
