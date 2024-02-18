494e3432df7d06bb0d1c7aa4685a561ed3cad52d
Abdurohman Karim <ghostmagic766@gmail.com>
2024-02-18 13:14:08 +0500

    validation added

--- routes/auth.js
+++ routes/auth.js
@@ -7,30 +7,75 @@ const router = Router();
 router.get("/login", (req, res) => {
     res.render("login", {
         title: "Login | My Store",
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError')
     })
 })
 router.get("/register", (req, res) => {
     res.render("register", {
         title: "Register | My Store",
-        isRegister: true
+        isRegister: true,
+        registerError: req.flash('registerError')
     })
 })
 
-router.post("/login", (req, res) => {
-    console.log(req.body);
+// Login
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email || !password) {
+        req.flash('loginError', 'All fields are required')
+        res.redirect("/login")
+        return
+    }
+
+    const existUser = await User.findOne({email})
+    if(!existUser) {
+        req.flash('loginError', 'User not found')
+        res.redirect('/login')
+        return
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, existUser.password);
+    if(!isPasswordValid) {
+        req.flash('loginError', 'Wrong password')
+        res.redirect('/login')
+        return
+    }
+
+    console.log(existUser);
     res.redirect("/")
 })
 
+// Register
 router.post("/register", async (req, res) => {
-    const hashPassword = await bcrypt.hash(req.body.password, 10)
+    const {email, password, full_name} = req.body
+
+    if(!email || !password || !full_name) {
+        req.flash('registerError', 'All fields are required')
+        res.redirect('/register')
+        return
+    }
+
+    if(password.length < 7) {
+        req.flash('registerError', 'Password must be 8 or more characters')
+        res.redirect('/register')
+        return
+    }
+
+    if(await User.findOne({email})) {
+        req.flash('registerError', 'User already exists')
+        res.redirect('/register')
+        return
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
     const userData = {
-        full_name: req.body.full_name,
-        email: req.body.email,
+        full_name: full_name,
+        email: email,
         password: hashPassword
     }
     const user = await User.create(userData)
-    console.log(user);
     res.redirect("/")
 })
 
